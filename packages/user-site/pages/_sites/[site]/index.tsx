ee0df1faffe123162db4c93766e1f5f44b3946fa
nettohadi <32300655+nettohadi@users.noreply.github.com>
2023-06-08 14:11:21 +0800

    chore: print data when not found

--- packages/user-site/pages/_sites/[site]/index.tsx
+++ packages/user-site/pages/_sites/[site]/index.tsx
@@ -19,7 +19,7 @@ export default function Index({
   error: ApiErrorType;
 }) {
   if (error?.code === pageIsNotFound || !data?.page) {
-    return <Page404 />;
+    return <Page404 data={data} />;
   }
 
   if (error?.code) {
